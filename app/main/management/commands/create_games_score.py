6b5f1caeb9d9b7c02bc3cb046e4010fde0b245d6
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2024-11-27 00:37:35 +0400

    fix create games score

--- app/main/management/commands/create_games_score.py
+++ app/main/management/commands/create_games_score.py
@@ -12,18 +12,16 @@ from tournament.models import Game
 class Command(BaseCommand):
     """ autocreate admin user """
     def handle(self, *args, **options):
-        second_score = None
         all_games = Game.objects.all()
         
         for game in all_games:
-            first_score = random.randint(0, 9)
+            first_score = random.randint(0, 3)
 
-            if first_score == 9:
-                second_score = random.randint(0, 8)
+            if first_score == 3:
+                second_score = random.randint(0, 2)
 
             else:
-                while (second_score == first_score and second_score is None):
-                    second_score = random.randint(first_score, 9)
+                second_score = 3
 
             game.first_player_score = first_score
             game.second_player_score = second_score
