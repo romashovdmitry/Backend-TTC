b4bf9eaf01b2609352af68b34ec79423659e9fc2
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2024-11-30 00:50:54 +0400

    update settings cookie

--- app/main/settings.py
+++ app/main/settings.py
@@ -280,7 +280,17 @@ CHANNEL_LAYERS = {
 
 
 SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
+    "http://localhost:3000",
+    "http://localhost/",
+    "http://localhost",
+    "http://127.0.0.1/",
+    "http://127.0.0.1",
+]
+
+CORS_ALLOW_HEADERS = ["*"]
